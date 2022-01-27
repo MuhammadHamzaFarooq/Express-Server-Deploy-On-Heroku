cc9ce8495a9fdb6d8f075af2d53d18de766d0c1e
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-01-27 17:34:02 +0500

    add user route

--- index.mjs
+++ index.mjs
@@ -3,9 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+let user = {
+    name: 'Muhammad Hamza Farooq',
+    age : 21,
+    city : "Karachi"
+}
 app.get('/',(req,res)=>{
     res.send("Express Server Deploy On Heroku")
-})
+});
+
+app.get('/user',(req,res)=>{
+    res.json({
+       message:"User Request Successfully Handle",
+       user 
+    })
+});
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is upon running on port ${PORT}`);
